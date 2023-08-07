1b9869b9b9629147a766069b7773257b243f0021
JackieC1993 <jackiecheung@pursuit.org>
2023-08-07 02:36:40 -0400

    added cohort dates

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import OneOnOneSection from "./Components/OneOnOneSection";
 import StudentCard from "./Components/StudentCard";
 import StudentDetails from "./Components/StudentDetails";
 import "./index.css";
+import CohortCard from "./Components/CohortCard";
 
 function App() {
 const studentCount = data.length;
  const student = data[0]
-    const studentsToRender = data.map(
+const [selectedCohort,setSelectedCohort] = useState('')
+const handleCohortClick = (cohortCode) => {
+  setSelectedCohort(cohortCode);
+};
+    const renderStudents = data.map(
       (eachStudentObj) => (
   
         <div className="card_container" key={eachStudentObj.id}>
@@ -32,24 +37,27 @@ const studentCount = data.length;
   )
   )
 
-  
-  
-  
-  
-  
-  
-  
-  
-
   return (
     <div className="app">
       <h1>Student Dashboard</h1>
-      <p>Number of Students: {studentCount}</p>
-      {studentsToRender}
+      <div className="cohort-links">
+        <h2>Choose a Class by Start Date</h2>
+        <a onClick={() => handleCohortClick('Spring2025')} href="#!">Spring 2025</a>
+        <a onClick={() => handleCohortClick('Summer2025')} href="#!">Summer 2025</a>
+        <a onClick={() => handleCohortClick('Fall2025')} href="#!">Fall 2025</a>
+        <a onClick={() => handleCohortClick('Winter2025')} href="#!">Winter 2025</a>
+        <a onClick={() => handleCohortClick('Spring2026')} href="#!">Spring 2026</a>
+        <a onClick={() => handleCohortClick('Summer2026')} href="#!">Summer 2026</a>
+        <a onClick={() => handleCohortClick('Fall2026')} href="#!">Fall 2026</a>
+        <a onClick={() => handleCohortClick('Winter2026')} href="#!">Winter2026</a>
+      </div>
+      <p>Number of Students: {data.length}</p>
+      {renderStudents}
      <OneOnOneSection/>
-      <StudentCard />
-      <StudentDetails/>
-    </div>
+      <StudentCard student={data[0]} />
+
+      {selectedCohort && <CohortCard cohortCode={selectedCohort} />}
+    </div> 
   );
 }
 export default App;
